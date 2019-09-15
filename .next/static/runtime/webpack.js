435c452867dbbe873a3383719b3e8212fa44fa00
AndresGLP <andres27l97@gmail.com>
2019-09-15 17:57:57 -0500

    hmm

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2a4346fa2b5443ba6ccd";
+/******/ 	var hotCurrentHash = "2d0984c0b4e4eb3f410f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
